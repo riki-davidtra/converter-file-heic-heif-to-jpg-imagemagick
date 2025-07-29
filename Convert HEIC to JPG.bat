99d5c9ca55cf8d5d7f93ec7c1602fbd2db0a2b22
Riki Davidtra <rikidavidtra.2310@gmail.com>
2025-07-29 13:29:36 +0700

    update

--- Convert HEIC to JPG.bat
+++ Convert HEIC to JPG.bat
@@ -1,34 +1,42 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Ensure folder is provided
-if "%~1"=="" (
-    echo Tidak ada folder target. Harap gunakan "Send to".
+REM ====== Validate only one selected folder ======
+REM Check the number of arguments
+if "%~2" neq "" (
+    echo Harap pilih hanya SATU folder saja.
     pause
     exit /b
 )
 
+REM Check if argument is a folder
+if not exist "%~1\" (
+    echo Yang Anda pilih bukan folder. Harap gunakan "Send to" pada folder.
+    pause
+    exit /b
+)
+
+REM Go to the selected folder
 cd /d "%~1"
 
-REM Check for HEIC files
-dir *.heic *.HEIC >nul 2>&1
+REM ====== Check HEIC or HEIF files ======
+dir *.heic *.HEIC *.heif *.HEIF >nul 2>&1
 if errorlevel 1 (
-    echo Tidak ada file .heic di folder ini.
+    echo Tidak ada file .heic atau .heif di folder ini.
     pause
     exit /b
 )
 
-echo File .heic ditemukan di folder: %~1
-
-echo. 
-set /p confirmConversion=Konversi semua file .heic ke .jpg? (y/n): 
+echo File .heic atau .heif ditemukan di folder: %~1
+echo.
+set /p confirmConversion=Konversi semua file .heic dan .heif ke .jpg? (y/n): 
 if /i not "%confirmConversion%"=="y" (
     echo Konversi dibatalkan.
     pause
     exit /b
 )
 
-REM Create folders if not exist
+REM ====== Create a trash and converted folder if you don't have one already. ======
 if not exist "trash" (
     mkdir "trash"
 )
@@ -36,12 +44,33 @@ if not exist "converted" (
     mkdir "converted"
 )
 
-REM Initialize counters
+REM ====== Counter initialization ======
 set successCount=0
-set failCount=0 
+set failCount=0
+
 
 echo Mengonversi...
 echo. 
+
+REM Loop untuk file heic dan heif (case insensitive)
+for %%f in (*.heic *.HEIC *.heif *.HEIF) do (
+    echo Mengonversi: %%f
+    magick "%%f" -quality 100 "converted\%%~nf.jpg"
+
+    if exist "converted\%%~nf.jpg" (  
+        echo "converted\%%~nf.jpg"
+        echo "trash\%%f"
+        echo Berhasil dikonversi. 
+        set /a successCount+=1
+        move "%%f" "trash\" >nul
+    ) else (
+        echo Gagal konversi: %%f
+        set /a failCount+=1
+    )
+    echo -----------------------------------------------
+)
+
+
 for %%f in (*.heic *.HEIC) do (
     echo Mengonversi: %%f
     magick "%%f" -quality 100 "converted\%%~nf.jpg"
